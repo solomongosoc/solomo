af77bd118f02805d1cb30f009da89baab2222ec7
solomongosoc <83348141+solomongosoc@users.noreply.github.com>
2021-04-28 19:50:56 +0800

    Add files via upload

---
+++ index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('he7v7s') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "he7v7s";  tokens.uclickhash = "he7v7s-he7v7s-ghg6-0-8pa2-bgwh-bgkt-a749e0"; 
